e4a3661a7064ed4370816671c0fee6268644d732
BIT Student <classroom@bgit.rs>
2018-03-20 18:24:19 +0100

    gotov projekat

--- w8/d1/projectmount/user-list/src/app/userList/userItem.js
+++ w8/d1/projectmount/user-list/src/app/userList/userItem.js
@@ -1,13 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types"
-
+import FormatDate from "./../../shared/utils/formatDate"
+import FormatEmail from "./../../shared/utils/formatEmail"
 
 const UserItem = (props) => {
+console.log('------------------------------------');
+console.log(props);
+console.log('------------------------------------');
     return (<div>
-        <p>{props.item.picture.thumbnail}</p>
+        <ul className="collection">
+      
+        <li className="collection-item avatar">
+        <img className="material-icons circle" src= {props.item.picture.thumbnail}></img>
+        <span className="title"></span>
+        <p>{props.item.name.first}<br/>
+        {FormatEmail(props.item.email)}<br/>
+        {"Date of birth: " + FormatDate(props.item.dob)}
+        </p>
+        <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
+      </li>
+      
+    </ul>
+    
+
+    
+
+
+        {/* <p>{props.item.picture.thumbnail}</p>
         <p>{props.item.name.first}</p>
         <p>{props.item.email}</p>
-        <p>{props.item.dob}</p>
+        <p>{props.item.dob}</p> */}
     </div>)
 };
 
